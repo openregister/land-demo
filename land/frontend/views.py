ea2085db3ab18ea03bb958b2282d70485bbcfb14
psd <psd@whatfettle.com>
2016-02-09 17:52:36 +0000

    Ignore demodata, register_view

--- land/frontend/views.py
+++ land/frontend/views.py
@@ -9,7 +9,17 @@ from flask import (
     url_for
 )
 
+import os
 import requests
+from openregister import Item
+from openregister.stores.mongodb import MongoStore
+
+mongo_host = os.getenv('DB_PORT_27017_TCP_ADDR', '127.0.0.1')
+mongo_uri = 'mongodb://%s:27017/landregistry' % mongo_host
+
+# load register register
+land_title_store = MongoStore(mongo_uri, prefix="land_title_")
+
 
 from land.frontend.forms import SearchForm
 
@@ -19,3 +29,8 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 @frontend.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@frontend.route('/land/title/<number>', methods=['GET'])
+def register_view(number):
+    meta, land_title = land_title_store.find(query={'land-title': number})
+    return render_template('register_view.html', land_title=land_title[0])
