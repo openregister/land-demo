8e84d8c36ca0c33039cd82df1bcf12c609b10c7e
psd <psd@whatfettle.com>
2016-02-10 23:09:19 +0000

    Load company names

--- land/frontend/views.py
+++ land/frontend/views.py
@@ -11,6 +11,7 @@ from flask import (
 )
 
 import os
+import re
 import requests
 from openregister.client import Client
 from openregister.stores.mongodb import MongoStore
@@ -20,6 +21,7 @@ mongo_uri = 'mongodb://%s:27017/landregistry' % mongo_host
 
 land_title_store = MongoStore(mongo_uri, prefix="land_title_")
 land_title_clause_store = MongoStore(mongo_uri, prefix="land_title_clause_")
+company_store = MongoStore(mongo_uri, prefix="company_")
 
 client = Client()
 
@@ -63,10 +65,18 @@ def register_view(number):
         }
     }
 
+    records = {}
+
+    address = client.item('address', land_title['address'])
+    records['address:' + address.address] = address.json
+
+    company_re = re.compile('\[\[company:([0-9]*)\]\]')
+
     for clause in clauses:
         clause['text'] = clause['text'].strip()
         sections[clause['land-title-section']]['clauses'].append(clause)
+        for number in company_re.findall(clause['text']):
+            meta, company = company_store.find(query={'company': number})
+            records['company:'+number] = company[0].json
 
-    address = client.item('address', land_title['address'])
-
-    return render_template('register_view.html', address=address, land_title=land_title, sections=sections, section_order=section_order)
+    return render_template('register_view.html', address=address, land_title=land_title, sections=sections, section_order=section_order, records=records)
