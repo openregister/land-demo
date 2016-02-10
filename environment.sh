b6173b7b5d2f0bae56308abcea059218b59fd0b4
psd <psd@whatfettle.com>
2016-02-10 06:28:24 +0000

    ADDRESS_REGISTER config

--- environment.sh
+++ environment.sh
@@ -1,4 +1,3 @@
 export SETTINGS='config.DevelopmentConfig'
-export PREMISES_REGISTER='http://premises.preview.openregister.org'
 export MONGO_URI='mongodb://localhost:27017'
-export PRODUCTS_OF_ANIMAL_ORIGIN_ACTIVITY='http://products-of-animal-origin-activity.openregister.org'
+export ADDRESS_REGISTER='http://address.openregister.org'
